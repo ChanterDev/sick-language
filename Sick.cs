3069cd41e291eb3f6c2451c023d17b9b518470b1
ChanterDev <matvey.dibrov@yandex.ru>
2025-03-05 12:31:43 +0300

    debug token display disable

--- Sick.cs
+++ Sick.cs
@@ -6,9 +6,11 @@ public class Sick {
         string content = File.ReadAllText(args[0]);
         Lexer lexer = new(content);
         List<Token> tokens = lexer.Tokenize();
+        /*
         foreach (Token token in tokens) {
             Console.WriteLine(token);
         }
+        */
         Parser parser = new(tokens);
         CompoundStatement compound = parser.Parse();
         compound.Execute();
